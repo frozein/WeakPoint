e0b0cf2898c174651063856a2b160e27a90fdf6a
sumandas27 <sumanta.das2727@gmail.com>
2022-11-20 13:01:41 -0500

    Destroys drone correctly now, added dash cooldown indicator

--- src/Scenes/GameScene.cpp
+++ src/Scenes/GameScene.cpp
@@ -6,9 +6,15 @@ GameScene::GameScene() {
 
     elements.push_back(std::make_shared<RedDrone>(playerPtr, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2));
 
-    scoreText = TextureAttributes(TEXTURE_MULTIPURPOSE_PIXEL, graphics::SRC_NULL, { 100, 100, 0, 0 }, 0.0, NULL, SDL_FLIP_NONE, { 255, 255, 255, 255 }, false, 2);
     score = "0";
+    scoreText = TextureAttributes(TEXTURE_MULTIPURPOSE_PIXEL, graphics::SRC_NULL, { 100, 100, 0, 0 }, 0.0, NULL, SDL_FLIP_NONE, { 255, 255, 255, 255 }, false, 2);
     elements.push_back(std::make_shared<VariableText>(&score, &scoreText));
+
+    cooldown = "Dash ready!";
+    cooldownText = TextureAttributes(TEXTURE_MULTIPURPOSE_PIXEL, graphics::SRC_NULL, { WINDOW_WIDTH / 2, WINDOW_HEIGHT - 200, 0, 0 }, 0.0, NULL, SDL_FLIP_NONE, { 255, 255, 255, 255 }, true, 1);
+    elements.push_back(std::make_shared<VariableText>(&cooldown, &cooldownText));
+    cooldownIndicator = TextureAttributes(TEXTURE_MULTIPURPOSE_PIXEL, graphics::SRC_NULL, { 0, WINDOW_HEIGHT - 100, 0, 100 }, 0.0, NULL, SDL_FLIP_NONE, { 255, 255, 255, 255 }, false, 1);
+    elements.push_back(std::make_shared<Texture>(&cooldownIndicator));
 
     //temp map just for testing:
     Map::TileType tiles[MAP_W * MAP_H];
@@ -29,10 +35,12 @@ GameScene::~GameScene()
 
 void GameScene::update(float dt)
 {
-    if (playerPtr->hp == 0)
-        std::cout << "game end\n";
+    /*if (playerPtr->hp == 0)
+        std::cout << "game end\n";*/
 
     score = std::to_string(playerPtr->score);
+    cooldown = playerPtr->dash.cooldown > 0.0f ? std::to_string(playerPtr->dash.cooldown).substr(0, 4) : "Dash ready!";
+    cooldownIndicator.dstRect.w = WINDOW_WIDTH * (playerPtr->dash.cooldown / 2.0f);
 
     Scene::update(dt);
 }
